b53836cafce8dbb115a31aaaee4b7650b38f0db3
EC2 Default User <ec2-user@ip-172-31-13-127.ec2.internal>
2021-02-09 19:38:29 +0000

    added text to index file

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ def index():
     )
 
 app.run(
-    port=int(os.getenv("PORT", "8080")),
-    host=os.getenv("IP", "0.0.0.0"),
-    debug = True
+    host=os.getenv('IP', '0.0.0.0'),
+    port=int(os.getenv('PORT', 8080)),
+    debug = False
 )
 
