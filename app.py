61575fc589f75471d5756e09c2767213db556c8a
EC2 Default User <ec2-user@ip-172-31-95-12.ec2.internal>
2021-02-09 21:12:18 +0000

    more fix

--- app.py
+++ app.py
@@ -7,19 +7,8 @@ app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
 @app.route("/")
 def index():
-<<<<<<< HEAD
     return render_template("index.html",
     image='static/patrick1.jpg')
-=======
-    professor = "Professor Agrawal"
-    t = datetime.datetime.now()
-    date = "Today is "+ t.strftime("%A") + " " + t.strftime("%B") + " " + t.strftime("%d") + ", " + t.strftime("%Y")
-    return render_template(
-        "index.html",
-        professor=professor,
-        date = date,
-    )
->>>>>>> 3c3bd4a4e02effae4112244a9ce036946ea78668
 
 app.run(
     host=os.getenv('IP', '0.0.0.0'),
