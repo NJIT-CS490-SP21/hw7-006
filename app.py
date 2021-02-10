5722c884efcbdebb7d7d228679c9da83da4a717c
EC2 Default User <ec2-user@ip-172-31-63-209.ec2.internal>
2021-02-10 23:11:04 +0000

    Julio's favorite shows, anime, and movies

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import datetime
 import random
 from flask import Flask, render_template
 
+Folder_Images = os.path.join('static', 'images')
+
 app = Flask(__name__)
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+app.config['UPLOAD_FOLDER'] = Folder_Images
 
 @app.route("/")
 def index():
@@ -12,10 +15,20 @@ def index():
     t = datetime.datetime.now()
     date = "Today is "+ t.strftime("%A") + " " + t.strftime("%B") + " " + t.strftime("%d") + ", " + t.strftime("%Y")
     number=random.randint(1,10)
+    shows = ['Band of Brothers', 'The Pacific', 'Fullmetal Alchemist Brotherhood', 'My Hero Academia', 'The Mandalorian']
+    Pic1 = os.path.join(app.config['UPLOAD_FOLDER'], 'band-of-brothers.jpg')
+    Pic2 = os.path.join(app.config['UPLOAD_FOLDER'], 'pacific.jpg')
+    Pic3 = os.path.join(app.config['UPLOAD_FOLDER'], 'Fullmetal.jpg')
+    Pic4 = os.path.join(app.config['UPLOAD_FOLDER'], 'hero.jpg')
+    Pic5 = os.path.join(app.config['UPLOAD_FOLDER'], 'Mandalorian.jpg')
+    pics_list = [Pic1, Pic2, Pic3, Pic4, Pic5]
+
     return render_template(
         "index.html",
         professor=professor,
         date = date,
+        my_shows_list = shows,
+        my_pic_list = pics_list,
         number=number
     )
 
