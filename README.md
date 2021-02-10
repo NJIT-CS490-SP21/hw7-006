743684b7488024eb0592f34e0fba8e3ffc44b789
EC2 Default User <ec2-user@ip-172-31-63-209.ec2.internal>
2021-02-10 01:38:48 +0000

    Inital Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### CS 490 HW7 - Section 006
+
+In order to get the app running in its base state, debug must be set to false and the port number 8080 must have the quotations marks around it removed.
---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+import datetime
+from flask import Flask, render_template
+
+Folder_Images = os.path.join('static', 'images')
+
+app = Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+app.config['UPLOAD_FOLDER'] = Folder_Images
+# comment
+@app.route("/")
+def index():
+    professor = "Professor Bob"
+    t = datetime.datetime.now()
+    date = "Today is "+ t.strftime("%A") + " " + t.strftime("%B") + " " + t.strftime("%d") + ", " + t.strftime("%Y")
+    shows = ['Band of Brothers', 'The Pacific', 'Fullmetal Alchemist Brotherhood', 'My Hero Academia', 'The Mandalorian']
+    Pic1 = os.path.join(app.config['UPLOAD_FOLDER'], 'band-of-brothers.jpg')
+    Pic2 = os.path.join(app.config['UPLOAD_FOLDER'], 'pacific.jpg')
+    Pic3 = os.path.join(app.config['UPLOAD_FOLDER'], 'Fullmetal.jpg')
+    Pic4 = os.path.join(app.config['UPLOAD_FOLDER'], 'hero.jpg')
+    Pic5 = os.path.join(app.config['UPLOAD_FOLDER'], 'Mandalorian.jpg')
+    pics_list = [Pic1, Pic2, Pic3, Pic4, Pic5]
+
+    return render_template(
+        "index.html",
+        professor=professor,
+        date = date,
+         my_shows_list = shows,
+        my_pic_list = pics_list
+    )
+
+app.run(
+    host=os.getenv('IP', '0.0.0.0'),
+    port=int(os.getenv('PORT', 8080)),
+    debug = False
+)
+
